f2f3dfd2c77c72f5ae2f414de5e8d70c71235206
daersh <daersh@naver.com>
2024-03-04 12:29:56 +0900

    로드 벨런싱 적용, 주석 추가

--- chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
+++ chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-//@RequestMapping("/first-service")
-@RequestMapping("/")
-public class FirstServiceController {
 
+//@RequestMapping("/first-service")     /*Gateway 추가 후 Routes 경로 추가 후 작성한 부분*/
+@RequestMapping("/")            /*Gateway에서 RewritePath 필터 추가 후 뒤에 segment만 넘어오게 한 이후 */
+public class FirstServiceController {
+    private Environment env;        //@Value와 같이 application.yml 또는 환경 설정 값을 불러오기 위함!!@!@!
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("health-check")
     public String healthCheck(){
-        return "I'm OK....!";
+//        log.info("요청 경로 확인: {}",env.getProperty("local.server.port"));
+//        log.info("요청 경로 확인: {}",env.getProperty("server.port"));
+        /*first-service application 여러 개 실행시(스케일 아웃) RR 방식으로 돌아가는 것을 포트 번호를 통해 확인할 수 있다.*/
+        /*Gateway의 lb(로드벨런싱)를 통해 RR 방식으로 스위칭 된다!!!!!*/
+        return "I'm OK....!\n port:"+env.getProperty("local.server.port");
     }
+    /*gateway를 거치고 넘어오는 요청 시 RequestHeader에 값이 추가 됨(Gateway의 필터에 의해)*/
     @GetMapping("message")
     public String message(@RequestHeader("first-request")String header){
         log.info("넘어온 헤더값: {}",header);
